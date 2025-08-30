337e640cb8fa2f672e258661eaa119a42b327f62
bharath <bharathh1817@gmail.com>
2025-08-30 17:11:31 +0530

    armstrong number

---
+++ armstrong.py
@@ -0,0 +1,25 @@
+def armstrong(num):
+  if num<0:
+    return False
+  original = num
+  num_digits = len(str(num))
+  arm_sum = 0
+
+  while num>0:
+    digit = num%10
+    arm_sum += digit**num_digits
+    num = num//10
+
+  return arm_sum == original
+
+try:
+  n = int(input("Enter a number: "))
+except ValueError:
+  print("Invalid! Please enter a valid number")
+
+if(n<0):
+  print("Please enter a positive integer")
+elif armstrong(n):
+  print(f"{n} is an armstrongnuber")
+else:
+  print(f"{n} is NOT an armstrong number")    
