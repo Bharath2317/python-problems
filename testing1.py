db1ca03ee8b983ab76a6045fbd11ed2b36458256
bharath <bharathh1817@gmail.com>
2025-09-10 22:31:58 +0530

    testing practice

---
+++ testing1.py
@@ -0,0 +1,7 @@
+def main():
+  name = input("Enter a name: ")
+  return name
+
+def hello(to="world"):
+  return f"Hello, {to}"
+
---
+++ testing2.py
@@ -0,0 +1,4 @@
+from testing1 import hello
+
+def testing():
+  assert hello("bharath") == "Hello, bharath"
